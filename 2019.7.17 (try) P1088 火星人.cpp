1af728e3e0cb9eea1090762939ce77b4a1dd0fe8
wanghouhuasteven <31465247+wanghouhuasteven@users.noreply.github.com>
2019-07-17 10:54:46 +0800

    Rename 2019.7.17 (try)P1088 火星人.cpp to 2019.7.17 (try) P1088 火星人.cpp

---
+++ 2019.7.17 (try) P1088 火星人.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    int f[10005];
+    cin >> n;
+    cin >> m;
+    for(int i=1;i<=n;i++) cin >> f[i];
+    return 0;
+}
